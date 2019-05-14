363e6763c8c3dffe2d134a7f27bf9e5ccb9b485b
Adam <amitrega01@gmail.com>
2019-05-14 21:09:05 +0200

    styles etc

--- form-react/src/reducer.js
+++ form-react/src/reducer.js
@@ -1,14 +1,31 @@
-import { stat } from 'fs';
-
 const initialState = {
   eventUsers: [],
 };
 export default function eventForm(state = initialState, action) {
   switch (action.type) {
     case 'SIGNUP': {
-      console.log('HELLo');
+      console.log('DISPATCHER');
+      let userString = JSON.stringify(action.user);
+      console.log(userString);
+      let data = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: userString,
+      };
+      fetch('http://localhost:3001/eventUsers', data)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if (data.err) alert(data.err);
+        });
       return { eventUsers: [...state.eventUsers, action.user] };
     }
+    case 'FROMDB': {
+      return { eventUsers: action.users };
+    }
+    default:
+      return state;
   }
-  return state;
 }
